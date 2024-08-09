baf0f3ca4f7019fb060b93eff626487aaa6d5e15
Ish1126 <156629367+Ish1126@users.noreply.github.com>
2024-08-09 19:15:52 -0400

    commit

--- poke_api.py
+++ poke_api.py
@@ -3,6 +3,8 @@ Library for interacting with the PokeAPI.
 https://pokeapi.co/
 '''
 import requests
+import os
+import image_lib
 
 POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
@@ -10,6 +12,7 @@ def main():
     # Test out the get_pokemon_into() function
     # Use breakpoints to view returned dictionary
     poke_info = get_pokemon_info("Rockruff")
+    download_pokemon_artwork('')
     return
 
 def get_pokemon_info(pokemon):
@@ -47,8 +50,42 @@ def get_pokemon_info(pokemon):
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
 
     # TODO: Define function that gets a list of all Pokemon names from the PokeAPI
-
+def get_pokemon_names(limit=100000, offset=0):
+    print(f'Getting list of Pokemon names.....', end='')
+    params= {
+        'limit': limit,
+        'offset': offset
+    }
+    resp_msg = requests.get(POKE_API_URL, params=params)
+    if resp_msg.status_code == requests.codes.ok:
+        print('Success')
+        resp_dict = resp_msg.jsob()
+        return [p['name'] for p in resp_dict['results']]
+    else:
+        print('Failure')
+        print(f'Response code: {resp_msg.status.code} ({resp_msg.reason})')
     # TODO: Define function that downloads and saves Pokemon artwork
+def download_pokemon_artwork(pokemon_name, folder_path='.')
+    poke_info = get_pokemon_info(pokemon_name)
+    if poke_info is None:
+        return
+    #Extract the wrtwork URL from the info dictionary
+    artwork_url = poke_info['sprites']['other']['official_artwork']['front-default']
+    if artwork_url is None:
+        print(f"No artwork available for (pokemon_name.capatalize()).")
+        return
 
+    #Determine the file path
+    file_ext= artwork_url.split('.'[-1])
+    image_path = os.path.join(folder_path.f'{pokemon_name}.{file_ext}')
+
+    #don't download Pokemon artwork if there already exists one
+    print(f'(Downloading artwork for {pokemon_name.capatalize()})')  
+    image_data = image_lib.download_image(artwork_url)
+    if image_data is None:
+        return
+    #Save the image file
+    if image_lib.save_image_file(image_data, image_path):
+        return image_path
 if __name__ == '__main__':
     main()
--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -9,8 +9,12 @@ Usage:
 from tkinter import *
 from tkinter import ttk
 import os
+import ctypes
+import inspect
+import image_lib
 
 # Get the script and images directory
+script_name= inspect......##TODO
 script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
@@ -19,11 +23,34 @@ images_dir = os.path.join(script_dir, 'images')
 # Create the main window
 root = Tk()
 root.title("Pokemon Viewer")
-
+root.geometry('600x600')
+root.minsize(500, 600)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
 # TODO: Set the icon
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('COMP593.PokeImageViewer')
+root.iconbitmap(os.path.join(script_dir, 'poke_ball.ico'))
 
 # TODO: Create frames
+frm = ttk.Frame(root) 
+frm.columnconfigure(0, weight=1)
+frm.rowconfigure(0, weight=1)
+frm.grid(sticky= NSEW)
 
 # TODO: Populate frames with widgets and define event handler functions
+image_path = os.path.join(script_dir, 'poke_ball.png')
+photo = PhotoImage(file=image_path)
+
+lbl_image = ttk.Label(frm, text='poke_ball.ico')
+lbl_image.grid(row=0, column=0, padx=10, pady=10)
+    
+#Create button to set desktop background 
+def handle_set_desktop():
+   
+    
+def handle_poke_sel(event):
+    
+cbox_poke_sel.bind('<<ComboBoxSelected>>', handle_poke_sel)    
+    
 
 root.mainloop()
